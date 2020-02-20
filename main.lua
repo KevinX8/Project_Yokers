043be741a7d30284c5b48134ddd3a6830a45aa4c
quinnpainter <quinn@geekymonkey.com>
2020-02-20 11:56:57 +0000

    Make player point at mouse cursor

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ physics.setGravity(0, 0)
 
 math.randomseed(os.time())
 
-local player = display.newImageRect("assets/player.png", 256, 256)
+local player = display.newImageRect("assets/player.png", 128, 128)
 physics.addBody(player, "dynamic")
 local playerSpeed = 250
 player.x = display.contentCenterX
@@ -14,7 +14,7 @@ local pressDown = false
 local pressLeft = false
 local pressRight = false
 
-local function key(event)
+local function keyEvent(event)
     local phase = event.phase
     local name = event.keyName
     local keyState = false
@@ -43,4 +43,10 @@ local function key(event)
     end
     player:setLinearVelocity(newXVelocity, newYVelocity)
 end
-Runtime:addEventListener("key", key)
+
+local function MouseEvent(event)
+    player.rotation = math.deg(math.atan2(player.y - event.y, player.x - event.x)) - 90
+end
+
+Runtime:addEventListener("key", keyEvent)
+Runtime:addEventListener("mouse", MouseEvent )
