247a4965c95aa5a5c5ff73fbe7d3412d10ca581a
quinnpainter <quinn@geekymonkey.com>
2020-02-25 11:56:57 +0000

    Move player code to seperate file + make controls configureable

--- main.lua
+++ main.lua
@@ -1,20 +1,12 @@
-local playerSpeed = 250
-local playerProjectileSpeed = 300
+Physics = require("physics")
+local player = require("scripts.player")
 
-local physics = require("physics")
-physics.start()
-physics.setGravity(0, 0)
+Physics.start()
+Physics.setGravity(0, 0)
 
 math.randomseed(os.time())
 
-local player = display.newImageRect("assets/player.png", 128, 128)
-physics.addBody(player, "dynamic")
-player.x = display.contentCenterX
-player.y = display.contentCenterY
-local pressUp = false
-local pressDown = false
-local pressLeft = false
-local pressRight = false
+player.start()
 
 --[[
 local fill = script.Parent.Filler
@@ -60,54 +52,13 @@ end
 
 --[[damage() Have to make function so damage can 
 //       actually be applied --]] 
-    
-local function throwProjectile()
-    local newProjectile = display.newImageRect("assets/egg.png", 64, 64)
-    physics.addBody(newProjectile, "dynamic", {isSensor=true})
-    newProjectile.isBullet = true -- makes collision detection "continuous" (more accurate)
-    newProjectile.myName = "playerProjectile" -- also used for collision detection
-    newProjectile.x = player.x
-    newProjectile.y = player.y
-    newProjectile.rotation = player.rotation
-    local angle = math.rad(newProjectile.rotation - 90) -- use the projectile's direction to see which way it should go
-    newProjectile:setLinearVelocity(math.cos(angle) * playerProjectileSpeed, math.sin(angle) * playerProjectileSpeed)
-end
 
 local function keyEvent(event)
-    local phase = event.phase
-    local name = event.keyName
-    local keyState = false
-    local newXVelocity, newYVelocity = 0, 0
-    if phase == "down" then keyState = true end
-    if name == "w" then
-        pressUp = keyState
-    elseif name == "s" then
-        pressDown = keyState
-    elseif name == "a" then
-        pressLeft = keyState
-    elseif name == "d" then
-        pressRight = keyState
-    end
-    if pressUp == true then
-        newYVelocity = newYVelocity - playerSpeed
-    end
-    if pressDown == true then
-        newYVelocity = newYVelocity + playerSpeed
-    end
-    if pressLeft == true then
-        newXVelocity = newXVelocity - playerSpeed
-    end
-    if pressRight == true then
-        newXVelocity = newXVelocity + playerSpeed
-    end
-    player:setLinearVelocity(newXVelocity, newYVelocity)
+    player.handleMovement(event)
 end
 
 local function MouseEvent(event)
-    player.rotation = math.deg(math.atan2(player.y - event.y, player.x - event.x)) - 90
-    if (event.isPrimaryButtonDown) then
-        throwProjectile()
-    end
+    player.handleMouse(event)
 end
 
 Runtime:addEventListener("key", keyEvent)
---
+++ scripts/player.lua
@@ -0,0 +1,82 @@
+local class = {}; -- this and the return statement at the end make this file behave somewhat like a class
+
+local playerSpeed = 250
+local playerProjectileSpeed = 300
+local upButton = "w"
+local downButton = "s"
+local leftButton = "a"
+local rightButton = "d"
+
+local playerImage
+
+local pressUp = false
+local pressDown = false
+local pressLeft = false
+local pressRight = false
+local mouseX = 0
+local mouseY = 0
+
+function class.start()
+    playerImage = display.newImageRect("assets/player.png", 128, 128)
+    Physics.addBody(playerImage, "dynamic")
+    playerImage.x = display.contentCenterX
+    playerImage.y = display.contentCenterY
+end
+
+function class.throwProjectile()
+    local newProjectile = display.newImageRect("assets/egg.png", 64, 64)
+    Physics.addBody(newProjectile, "dynamic", {isSensor=true})
+    newProjectile.isBullet = true -- makes collision detection "continuous" (more accurate)
+    newProjectile.myName = "playerProjectile" -- also used for collision detection
+    newProjectile.x = playerImage.x
+    newProjectile.y = playerImage.y
+    newProjectile.rotation = playerImage.rotation
+    local angle = math.rad(newProjectile.rotation - 90) -- use the projectile's direction to see which way it should go
+    newProjectile:setLinearVelocity(math.cos(angle) * playerProjectileSpeed, math.sin(angle) * playerProjectileSpeed)
+end
+
+function class.handleMovement(event)
+    local phase = event.phase
+    local name = event.keyName
+    local keyState = false
+    local newXVelocity, newYVelocity = 0, 0
+    if phase == "down" then keyState = true end
+    if name == upButton then
+        pressUp = keyState
+    elseif name == downButton then
+        pressDown = keyState
+    elseif name == leftButton then
+        pressLeft = keyState
+    elseif name == rightButton then
+        pressRight = keyState
+    end
+    if pressUp == true then
+        newYVelocity = newYVelocity - playerSpeed
+    end
+    if pressDown == true then
+        newYVelocity = newYVelocity + playerSpeed
+    end
+    if pressLeft == true then
+        newXVelocity = newXVelocity - playerSpeed
+    end
+    if pressRight == true then
+        newXVelocity = newXVelocity + playerSpeed
+    end
+    playerImage:setLinearVelocity(newXVelocity, newYVelocity)
+    class.updateRotation()
+end
+
+function class.handleMouse(event)
+    mouseX = event.x
+    mouseY = event.y
+    class.updateRotation()
+    if (event.isPrimaryButtonDown) then
+        class.throwProjectile()
+    end
+end
+
+function class.updateRotation()
+    playerImage.rotation = math.deg(math.atan2(playerImage.y - mouseY, playerImage.x - mouseX)) - 90
+end
+
+return class
