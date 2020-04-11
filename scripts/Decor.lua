b075f68e1bf278b3a29fe320a905e3a2fe89b430
t9rea <t9reaney@gmail.com>
2020-04-12 00:42:08 +0100

    fixed a layering issue

--- scripts/Decor.lua
+++ scripts/Decor.lua
@@ -22,7 +22,7 @@ function Decor.level1()
     local bushlimit = math.random(5,10)
     repeat--level1--
         local bush = display.newImageRect(BackgroundGroup, "assets/bush.png", 256, 256)
-        BackgroundGroup:insert(16,bush)
+        BackgroundGroup:insert(15,bush)
         Physics.addBody(bush, "dynamic", {radius=60, density=99999999.0})
         bush.myName = "bush"
         bush.x = math.random(LevelBoundLeft + 128,LevelBoundRight - 128)
@@ -38,7 +38,7 @@ function Decor.level2()
     local cactusLimit = math.random(8,13)
     repeat--level2--
         local cactus = display.newImageRect(BackgroundGroup, "assets/cactus.png", 256, 256)
-        BackgroundGroup:insert(16, cactus)
+        BackgroundGroup:insert(15, cactus)
         Physics.addBody(cactus, "dynamic", {radius=120, density=999999999.0})
         cactus.x = math.random(LevelBoundLeft + 128,LevelBoundRight - 128)+2872
         cactus.y = math.random(LevelBoundTop + 128,LevelBoundBottom - 128)
@@ -55,7 +55,7 @@ function Decor.level3()
     repeat
         local size = math.random(512, 768)
         local iceLake = display.newImageRect(BackgroundGroup, "assets/ice lake.png", size, size)
-        BackgroundGroup:insert(16, iceLake)
+        BackgroundGroup:insert(15, iceLake)
         Physics.addBody(iceLake, "static", {radius=size/2, density=999999999.0, isSensor=true})
         local positionValid = false
         local x = 0
@@ -92,7 +92,7 @@ function Decor.level4()
     repeat
         local size = math.random(512, 768)
         local lavaLake = display.newImageRect(BackgroundGroup, "assets/lava.png", size, size)
-        BackgroundGroup:insert(16, lavaLake)
+        BackgroundGroup:insert(15, lavaLake)
         Physics.addBody(lavaLake, "static", {radius=(size-30)/2, density=999999999.0, isSensor=true})
         local positionValid = false
         local x = 0
@@ -225,7 +225,7 @@ end
 
 function SpawnFireEggImage(egg)
     local fireEggImage = display.newImageRect(BackgroundGroup, "assets/fireegg.png", 380 / 8, 380 / 4)
-    BackgroundGroup:insert(16+iceLimit+lavaLimit,fireEggImage)
+    BackgroundGroup:insert(15+iceLimit+lavaLimit,fireEggImage)
     fireEggImage.rotation = egg.rotation + 180
     egg.fireEggImage = fireEggImage
     egg.alpha = 0.0
--- scripts/enemy.lua
+++ scripts/enemy.lua
@@ -39,7 +39,7 @@ function enemy.new(startX, startY)
         self.enemyImage.Enemytype = 0
         self.enemyImage.health = 1
     end
-    BackgroundGroup:insert(16+iceLimit+lavaLimit,self.enemyImage)
+    BackgroundGroup:insert(15+iceLimit+lavaLimit,self.enemyImage)
     self.enemyImage.instance = self -- give the image a reference to this script instance for collisionEvent
     Physics.addBody(self.enemyImage, "dynamic")
     self.enemyImage.myName = "enemy"
