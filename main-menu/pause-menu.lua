db342a773a340d69cfb5a61acc21727efc2c7f54
ENI6M4 <kevinmccarthy501@protonmail.com>
2020-04-15 21:28:53 +0100

    Update pause-menu.lua

--- main-menu/pause-menu.lua
+++ main-menu/pause-menu.lua
@@ -1,19 +1,33 @@
 local composer = require("composer")
 local scene = composer.newScene()
-local menuMusic
-local audioClick
 
 function scene:create(event)
    
     local sceneGroup = self.view
+        
+        local widget = require("widget")
 
-   menuMusic = audio.loadStream("Audio/)
+        local background = display.newImageRect("Images/Title_Screen.png", 1280, 720)
+        background.x = display.contentCenterX
+        background.y = display.contentCenterY
 
-   audioClick = audio.loadSound("Audio/)
-    
-end
+        local function closeGame(event)
+            native.requestExit()
+        end
 
-function pause(event)
+        local function goToGame(event)
+            if ("ended" == event.phase) then
+                composer.gotoScene("game", {effect = "crossFade", time = 500})
+            end
+        end
+ 
+        local function goToOptions(event)
+            if ("ended" == event.phase) then
+                composer.gotoScene("options", {effect = "crossFade", time = 500})
+            end
+        end
+
+	--[[local function pause(event)
     if event.phase == "began" then 
         if paused == false then
             physics.pause()
@@ -25,53 +39,28 @@ function pause(event)
     end
 end
 
-function scene:show(event)
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    if (phase == "will") then
-        
-        local widget = require("widget")
-
-        local background = display.newImageRect("Images/Title_Screen.png", 1280, 720)
-        background.x = display.contentCenterX
-        background.y = display.contentCenterY
-
-        local function closeGame()
-            native.requestExit()
-        end
-
-        local function goToLevelSelect(event)
-            if ("ended" == event.phase) then
-                composer.gotoScene("levelSelect", {effect = "crossFade", time = 500})
-            end
-        end
- 
-        local function goToOptions(event)
-            if ("ended" == event.phase) then
-                composer.gotoScene("options", {effect = "crossFade", time = 500})
-            end
-        end
+paused = false
+Runtime:addEventListener("touch", pause) --]]
 
         local newGame =
             widget.newButton(
             {
                 width = 289,
                 height = 40,
-                left = 240,
-                top = 640,
+                left = 825,
+                top = 630,
                 id = "newGame",
                 defaultFile = "Images/new_game.png",
-                onEvent = goToLevelSelect
+                onEvent = goToGame
             }
         )
 
         local resume =
             widget.newButton(
             {
-                width = 695,
-                height = 155,
-                left = 240,
+                width = 289,
+                height = 40,
+                left = 825,
                 top = 670,
                 id = "resume",
                 defaultFile = "Images/resume_Game.png",
@@ -84,26 +73,28 @@ function scene:show(event)
             {
                 width = 289,
                 height = 40,
-                left = 240,
+                left = 825,
                 top = 710,
                 id = "options",
                 defaultFile = "Images/options.png",
                 onEvent = goToOptions  
             }
+        )
             
         local quit =
             widget.newButton(
             {
                 width = 289,
                 height = 40,
-                left = 240,
+                left = 825,
                 top = 750,
                 id = "quit",
                 defaultFile = "Images/quit_game.png",
                 onEvent = closeGame
                 
             }
-        }
+        )
+        
 		sceneGroup:insert(background)
 		sceneGroup:insert(newGame)
 		sceneGroup:insert(resume)
@@ -111,7 +102,6 @@ function scene:show(event)
 		sceneGroup:insert(quit)
 
     end
-end
 
 function scene:show(event)
     local sceneGroup = self.view
@@ -121,6 +111,7 @@ function scene:show(event)
     elseif (phase == "did") then
 
         audio.play(menuMusic, {channel = 1, loops = -1})
+        
     end
 end
 
