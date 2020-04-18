38bb71892272278e673f6d438e01950ff6c711c6
ENI6M4 <kevinmccarthy501@protonmail.com>
2020-04-18 19:14:15 +0100

    Working properly

--- main-menu/optionsMenu.lua
+++ main-menu/optionsMenu.lua
@@ -1,114 +1,127 @@
 local composer = require("composer")
-local options = composer.newScene()
+local optionsMenu = composer.newScene()
+local sceneGroup
+local muteSoundText
+local volUpText
+local volDownText
+local backText
+local hidden = true
+
+Ponyfont = require "com.ponywolf.ponyfont" -- https://github.com/ponywolf/ponyfont used to load bitmap fonts (white bg)
+
 
 local function goToMenu(event)
-	composer.gotoScene("menu")
+	if(not hidden) then
+          composer.gotoScene("menu")
+       end
 end
        
 local function volumeUp(event)
-	audio.setVolume(audio.getVolume() + 0.1)
+	if(not hidden) then
+          audio.setVolume(audio.getVolume() + 0.1)
+       end
 end
 
 local function volumeDown(event)
-	audio.setVolume(audio.getVolume() - 0.1)
-end     
-
-function options:create(event)
-    
-	local sceneGroup = self.view
-
-	local background = display.newImageRect(sceneGroup, "assets/Title Screen.png", 1920, 1080)
-	background.x = display.contentCenterX
-	background.y = display.contentCenterY
-
-	local muteSound = display.newImageRect(sceneGroup, "Assets/blank.png", 500, 75)
-	muteSound.x = display.contentCenterX
-	muteSound.y = 655
-	local muteSoundText = Ponyfont.newText({
-	text = "Mute Sound",
-	x = muteSound.x,
-	y = muteSound.y,
-	font = "assets/coolfont.fnt",
-	fontSize = 32,
-	align = "centre"
-	})
-
-	local volUp = display.newImageRect(sceneGroup, "Assets/blank.png", 500, 75)
-	volUp.x = display.contentCenterX
-	volUp.y = 755
-	local volUpText = Ponyfont.newText({
-				text = "Volume Down",
-	x = volUp.x,
-	y = volUp.y,
-	font = "assets/coolfont.fnt",
-	fontSize = 32,
-		align = "centre"
-	})
-				
-	local volDown = display.newImageRect(sceneGroup, "Assets/blank.png",  500, 75)
-	volDown.x = display.contentCenterX
-	volDown.y = 855
-	local volDownText = Ponyfont.newText({
-	text = "Volume Down",
-	x = volDown.x,
-	y = volDown.y,
-	font = "assets/coolfont.fnt",
-	fontSize = 32,
-	align = "centre"
-	})
-
-	local back = display.newImageRect(sceneGroup, "Assets/blank.png",  500, 75)
-	back.x = display.contentCenterX
-	back.y = 955
-	local backText = Ponyfont.newText({
-	text = "Back",
-	x = back.x,
-	y = back.y,
-	font = "assets/coolfont.fnt",
-	fontSize = 32,
-	align = "centre"
-	})
-		
-	muteSound:addEventListener("tap", muteSound)
-	volUp:addEventListener("tap", volumeUp)
-	volDown:addEventListener("tap", volumeDown)
-	back:addEventListener("tap", goToMenu)
+	if(not hidden) then
+          audio.setVolume(audio.getVolume() - 0.1)
+       end 
 end
 
-function options:show(event)
-    local sceneGroup = self.view
-    local phase = event.phase
+function optionsMenu:create(event)
+     
+	sceneGroup = self.view
 
-	if (phase == "will") then
-	
-	elseif (phase == "did") then
-	
-	end
- end
+        local background = display.newImageRect(sceneGroup, "assets/Title Screen.png", 1920, 1080)
+			  background.x = display.contentCenterX
+			  background.y = display.contentCenterY
 
+	    local muteSound = display.newImageRect(sceneGroup, "Assets/blank.png", 500, 75)
+			  muteSound.x = display.contentCenterX
+			  muteSound.y = 655
+			  muteSoundText = Ponyfont.newText({
+			  text = "Mute Sound",
+			  x = muteSound.x,
+			  y = muteSound.y,
+			  font = "assets/coolfont.fnt",
+		  	  fontSize = 32,
+		 	  align = "centre"
+		      })
 
-function options:hide(event)
-    local sceneGroup = self.view
-    local phase = event.phase
+	    local volUp = display.newImageRect(sceneGroup, "Assets/blank.png", 500, 75)
+			  volUp.x = display.contentCenterX
+			  volUp.y = 755
+			  volUpText = Ponyfont.newText({
+              text = "Volume Up",
+			  x = volUp.x,
+			  y = volUp.y,
+			  font = "assets/coolfont.fnt",
+			  fontSize = 32,
+		      align = "centre"
+		      })
+			
+	    local volDown = display.newImageRect(sceneGroup, "Assets/blank.png",  500, 75)
+			  volDown.x = display.contentCenterX
+			  volDown.y = 855
+			  volDownText = Ponyfont.newText({
+			  text = "Volume Down",
+			  x = volDown.x,
+			  y = volDown.y,
+			  font = "assets/coolfont.fnt",
+			  fontSize = 32,
+	 		  align = "centre"
+		     })
 
-    if (phase == "will") then
-      
-    elseif (phase == "did") then
-		composer.removeScene("optionsMenu")
-    end
+	    local back = display.newImageRect(sceneGroup, "Assets/blank.png",  500, 75)
+			  back.x = display.contentCenterX
+			  back.y = 955
+			  backText = Ponyfont.newText({
+			  text = "Back",
+			  x = back.x,
+			  y = back.y,
+			  font = "assets/coolfont.fnt",
+			  fontSize = 32,
+			  align = "centre"
+		     })
+			
+			  muteSound:addEventListener("tap", muteSound)
+			  volUp:addEventListener("tap", volumeUp)
+			  volDown:addEventListener("tap", volumeDown)
+			  back:addEventListener("tap", goToMenu)
+		  end
+
+function optionsMenu:show(event)
+    muteSoundText.text = "Mute Sound"
+    volUpText.text = "Volume Up"
+    volDownText.text = "Volume Down"
+    backText.text = "Back"
+    hidden = false
 end
 
-function options:destroy(event)
-    local sceneGroup = self.view
+
+function optionsMenu:hide(event)
+    muteSoundText.text = ""
+    volUpText.text = ""
+    volDownText.text = ""
+    backText.text = ""
+    hidden = true
+end
+
+function optionsMenu:destroy(event)
+	sceneGroup:removeSelf()
+	muteSoundText.text = ""
+	volUpText.text = ""
+	volDownText.text = ""
+	backText.text = ""
 end
 
 -- -----------------------------------------------------------------------------------
 -- Scene event function listeners
 -- -----------------------------------------------------------------------------------
-options:addEventListener("create", options)
-options:addEventListener("show", options)
-options:addEventListener("hide", options)
-options:addEventListener("destroy", options)
+optionsMenu:addEventListener("create", optionsMenu)
+optionsMenu:addEventListener("show", optionsMenu)
+optionsMenu:addEventListener("hide", optionsMenu)
+optionsMenu:addEventListener("destroy", optionsMenu)
 -- -----------------------------------------------------------------------------------
 
-return options
+return optionsMenu
